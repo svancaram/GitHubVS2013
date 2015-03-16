ac2579a8e017267aa83cd889029a7d1047c180ad
svancaram <michl.svancara22@gmail.com>
2015-03-16 11:00:20 +0100

    vytvořen design obrazovky pro login do SW

--- EvidenceZakazek/Form1.Designer.vb
+++ EvidenceZakazek/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frm_login
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,88 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lbl_login = New System.Windows.Forms.Label()
+        Me.lbl_password = New System.Windows.Forms.Label()
+        Me.txt_name = New System.Windows.Forms.TextBox()
+        Me.txt_password = New System.Windows.Forms.MaskedTextBox()
+        Me.btn_OK = New System.Windows.Forms.Button()
+        Me.btn_Cancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_login
+        '
+        Me.lbl_login.AutoSize = True
+        Me.lbl_login.Location = New System.Drawing.Point(37, 35)
+        Me.lbl_login.Name = "lbl_login"
+        Me.lbl_login.Size = New System.Drawing.Size(33, 13)
+        Me.lbl_login.TabIndex = 0
+        Me.lbl_login.Text = "Login"
+        '
+        'lbl_password
+        '
+        Me.lbl_password.AutoSize = True
+        Me.lbl_password.Location = New System.Drawing.Point(37, 69)
+        Me.lbl_password.Name = "lbl_password"
+        Me.lbl_password.Size = New System.Drawing.Size(34, 13)
+        Me.lbl_password.TabIndex = 1
+        Me.lbl_password.Text = "Heslo"
+        '
+        'txt_name
+        '
+        Me.txt_name.Location = New System.Drawing.Point(96, 28)
+        Me.txt_name.Name = "txt_name"
+        Me.txt_name.Size = New System.Drawing.Size(177, 20)
+        Me.txt_name.TabIndex = 2
+        '
+        'txt_password
+        '
+        Me.txt_password.Location = New System.Drawing.Point(96, 62)
+        Me.txt_password.Name = "txt_password"
+        Me.txt_password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_password.Size = New System.Drawing.Size(177, 20)
+        Me.txt_password.TabIndex = 3
+        '
+        'btn_OK
+        '
+        Me.btn_OK.AllowDrop = True
+        Me.btn_OK.Location = New System.Drawing.Point(96, 123)
+        Me.btn_OK.Name = "btn_OK"
+        Me.btn_OK.Size = New System.Drawing.Size(84, 38)
+        Me.btn_OK.TabIndex = 4
+        Me.btn_OK.Text = "OK"
+        Me.btn_OK.UseVisualStyleBackColor = True
+        '
+        'btn_Cancel
+        '
+        Me.btn_Cancel.Location = New System.Drawing.Point(186, 122)
+        Me.btn_Cancel.Name = "btn_Cancel"
+        Me.btn_Cancel.Size = New System.Drawing.Size(86, 38)
+        Me.btn_Cancel.TabIndex = 5
+        Me.btn_Cancel.Text = "Cancel"
+        Me.btn_Cancel.UseVisualStyleBackColor = True
+        '
+        'frm_login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(313, 173)
+        Me.Controls.Add(Me.btn_Cancel)
+        Me.Controls.Add(Me.btn_OK)
+        Me.Controls.Add(Me.txt_password)
+        Me.Controls.Add(Me.txt_name)
+        Me.Controls.Add(Me.lbl_password)
+        Me.Controls.Add(Me.lbl_login)
+        Me.Name = "frm_login"
+        Me.Text = "Evidence zakázek - Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lbl_login As System.Windows.Forms.Label
+    Friend WithEvents lbl_password As System.Windows.Forms.Label
+    Friend WithEvents txt_name As System.Windows.Forms.TextBox
+    Friend WithEvents txt_password As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents btn_OK As System.Windows.Forms.Button
+    Friend WithEvents btn_Cancel As System.Windows.Forms.Button
 
 End Class
--- EvidenceZakazek/Form1.vb
+++ EvidenceZakazek/Form1.vb
@@ -1,3 +1,7 @@
-﻿Public Class Form1
+﻿Public Class frm_login
 
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
+        Application.Exit()
+
+    End Sub
 End Class
